7c126fca2a90389901693136fe3e09cc6b915c63
SabujMahonto <64306095+SabujMahonto@users.noreply.github.com>
2022-01-27 00:40:42 +0600

    conversion parseInt or parsefloat learning

---
+++ conversion.js
@@ -0,0 +1,9 @@
+var mangoPrice = "22.20"
+var orangePrice = "33";
+
+// var mangoPriceNum = parseFloat(mangoPrice);
+var mangoPriceNum = parseInt(mangoPrice);
+var orangePriceNum = parseFloat(orangePrice);
+
+console.log(mangoPriceNum);
+console.log(orangePriceNum);
