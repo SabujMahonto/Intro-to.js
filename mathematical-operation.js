764e4fc25b6fad94c690485af419ec3c866465ed
SabujMahonto <64306095+SabujMahonto@users.noreply.github.com>
2022-01-26 23:24:18 +0600

    This name are noe allow in js

---
+++ mathematical-operation.js
@@ -0,0 +1,15 @@
+var onionPrice = 43;
+var eggPrice = 42;
+eggQuantity = 7;
+// +
+var totalPrice = onionPrice + eggPrice;
+console.log(onionPrice);
+// -
+var priceDifference = onionPrice - eggPrice;
+// *
+var eggPriceMultiplication = eggPrice * eggQuantity;
+console.log(eggPriceMultiplication);
+// /
+var moneyAvailable = 100;
+var mangoPrice = 5;
+var mangoQuantity = moneyAvailable / mangoPrice;
---
+++ variable-name.js
@@ -0,0 +1,15 @@
+var name = "jon Dau"
+
+// reserved keyword not allow
+var false = 15;
+var delete = "delete"
+
+// name should be one word 
+var artist name = "Tom HANKS"
+
+ 
+// quote is not allow
+var "Movise" = "Hally poter"
+
+// name not allow 
+var 99price =99;
