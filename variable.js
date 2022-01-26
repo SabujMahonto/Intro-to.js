fa89d1c0c846f11d51e7d49ad31f108f6395e991
SabujMahonto <64306095+SabujMahonto@users.noreply.github.com>
2022-01-26 21:59:45 +0600

    type of variable

--- variable.js
+++ variable.js
@@ -1,5 +1,17 @@
+// numeric / Number
+var age = 26;
+var weight = 62;
+
+// String
 var firstName = "Sabuj"
 var lastName = "Mahonto"
-var age = "26"
+var address = "Dinajpur Vuter goli"
+var school = "IDAl School and collage"
+var gf = "Kim Mou"
 var fullName = firstName + " "+ lastName + " "+ age ;
 console.log(fullName);
+
+// Boolean
+var married = false;
+var student = true;
+
