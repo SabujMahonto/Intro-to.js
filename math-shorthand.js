176c6c6174ddea8b02536c3e256c08d9f2bf6f5a
SabujMahonto <64306095+SabujMahonto@users.noreply.github.com>
2022-01-27 00:11:48 +0600

    js shorthand

---
+++ math-shorthand.js
@@ -0,0 +1,32 @@
+var price1 = 31;
+var price2 = 10;
+// var sum = price + price2;
+// // var 	Subtraction
+// var subtraction = price2 - price;
+// var multiplication = price * price2;
+// var division = price1 / price2;
+// console.log(sum);
+// console.log(subtraction);
+// console.log(multiplication);
+// console.log(division);
+
+// short hand
+var newPrice = price1 + 10 ;
+price1 = price1 + 10 ;
+price1 += 44;
+
+var newPrice2 = price2 -5;
+price2 = price2 - 5;
+price2 -= 5;
+
+var age = 20;
+age = age + 1;
+age += 1;
+age++;
+console.log(age);
+var love = 100;
+love = love -1;
+love -= 1;
+love --;
+
+console.log(love);
--- variable.js
+++ variable.js
@@ -3,13 +3,13 @@ var age = 26;
 var weight = 62;
 
 // String
-var firstName = "Sabuj"
-var lastName = "Mahonto"
-var address = "Dinajpur Vuter goli"
-var school = "IDAl School and collage"
-var gf = "Kim Mou"
-var fullName = firstName + " "+ lastName + " "+ age ;
-console.log(fullName);
+// var firstName = "Sabuj"
+// var lastName = "Mahonto"
+// var address = "Dinajpur Vuter goli"
+// var school = "IDAl School and collage"
+// var gf = "Kim Mou"
+// var fullName = firstName + " "+ lastName + " "+ age ;
+// console.log(fullName);
 
 // Boolean
 var married = false;
