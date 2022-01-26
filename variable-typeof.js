fd2319ca56ed7c78bf1a6df9e7e87a1059ccb924
SabujMahonto <64306095+SabujMahonto@users.noreply.github.com>
2022-01-27 00:54:39 +0600

    all typeof learning done

---
+++ variable-typeof.js
@@ -0,0 +1,12 @@
+var mangoPrice = "200"
+var orangePrice = 400;
+var applePrice;
+var today = new Date()
+
+var isRipe = true;
+
+console.log(typeof mangoPrice);
+console.log(typeof orangePrice);
+console.log(typeof isRipe);
+console.log(typeof applePrice);
+console.log(typeof today);
