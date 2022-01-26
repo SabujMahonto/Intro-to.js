a1e904458703358a4b4b8ccd40845a612309d5f8
SabujMahonto <64306095+SabujMahonto@users.noreply.github.com>
2022-01-26 23:34:14 +0600

    mathmatical operation complite

--- mathematical-operation.js
+++ mathematical-operation.js
@@ -1,15 +1,15 @@
 var onionPrice = 43;
 var eggPrice = 42;
 eggQuantity = 7;
-// +
+// +	Addition
 var totalPrice = onionPrice + eggPrice;
 console.log(onionPrice);
-// -
+// - Subtraction
 var priceDifference = onionPrice - eggPrice;
-// *
+// * multiplication
 var eggPriceMultiplication = eggPrice * eggQuantity;
 console.log(eggPriceMultiplication);
-// /
+// /Division
 var moneyAvailable = 100;
 var mangoPrice = 5;
 var mangoQuantity = moneyAvailable / mangoPrice;
